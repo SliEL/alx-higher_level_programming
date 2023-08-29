a230e63ff049ae3fd232ec66eb56fc196211b1a9
Sliman Elmansouri <elmansourisliman@gmail.com>
2023-08-29 14:03:09 +0100

    mandatory tasks for 0x06 Classes project

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
 
 class Square:
-    """Represents a square.
-    Args:
-        int (size): the size of the new square.
+    """Represent a square."""
 
-    """
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
