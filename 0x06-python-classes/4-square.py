340d970e5585f2c566974ae9af986acbb8dcdf0d
Sliman Elmansouri <elmansourisliman@gmail.com>
2023-08-29 14:08:30 +0100

    mandatory tasks for 0x06 Classes project

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
+"""Defines a class Square."""
+
 
 class Square:
-    """Represent a square"""
-    def __init__(self, size = 0) -> None:
-        """Initialize a square
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+
         Args:
-            int (size): the size of the new square
+            size (int): The size of the new square.
         """
-        self.__size = size
-    
+        self.size = size
+
     @property
     def size(self):
-        """retrieves the current size of the square"""
+        """Gets and sets the current size of the square."""
         return (self.__size)
-    
+
     @size.setter
     def size(self, value):
-        """sets a new size for the square"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def area(self):
-        """Returns the area of the square"""
+        """Returns the current area of the square."""
         return (self.__size * self.__size)
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,39 +1,39 @@
 #!/usr/bin/python3
+"""Defines a class Square."""
+
 
 class Square:
-    """Represents a square"""
-    
-    def __init__(self, size = 0):
-        """Initialize the square
-        Args: 
-            size (int): the size of the new square
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes a new square.
+
+        Args:
+            size (int): The size of the new square.
         """
         self.size = size
 
     @property
     def size(self):
-        """retrieves the current size of the square"""
+        """Get and set the current size of the square."""
         return (self.__size)
 
     @size.setter
     def size(self, value):
-        """sets a new size for the square"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
-    def area(self):
-        """returns the area of the square"""
-        return (self.__size * self.__size)
-    
-    def my_print(self):
-        """prints the size of the square"""
-        for _  in range(0, self.__size):
-            [print("#", end="") for _ in range(0, self.__size)]
-            print("")
-        if (self.__size == 0):
-            print("")
 
-    
+    def area(self):
+        """Returns the current area of the square."""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Print the square with the # character."""
+        for _ in range(0, self.__size):
+            [print("#", end="") for _ in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
