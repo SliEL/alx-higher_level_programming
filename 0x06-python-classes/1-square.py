cd3e2e78c824502d47b33e5cdcd50658d57bb45e
Sliman Elmansouri <elmansourisliman@gmail.com>
2023-08-29 14:02:05 +0100

    mandatory tasks for 0x06 Classes project

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+"""Definition of a class Square."""
+
 
 class Square:
-    """Represent a square
-    Args:
-        int (size): the size of the new square.
-    """
+    """Represents a square."""
+
     def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
