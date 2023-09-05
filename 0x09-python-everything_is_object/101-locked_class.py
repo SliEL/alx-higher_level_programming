d79e03e93ec7240e10c91c60225ff72f92d515cd
Sliman Elmansouri <elmansourisliman@gmail.com>
2023-09-06 00:25:27 +0100

    0x09 everything is object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module contains a class that restricts 
+the creation of dynamic attributes.
+"""
+
+
+class LockedClass:
+    """Class that restricts dynamic attribute creation.
+
+    Attributes:
+        first_name (str): The first name for class instances.
+    """
+
+    __slots__ = ["first_name"]
