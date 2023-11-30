25a31844e69be3ab2cbf934260ad389dc18c00ca
Sliman Elmansouri <elmansourisliman@gmail.com>
2023-11-30 19:49:07 +0100

    task 1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,14 +2,4 @@
 # Script to get the size  of the body response
 
 
-# Check if the URL is provided as an argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Use curl to send a request to the provided URL and display the size of the body in bytes
-body_size=$(curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\r\n')
-
-# Display the size
-echo "$body_size"
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
