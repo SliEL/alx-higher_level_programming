5f3168155e5b3146411aea00bb32534c2d77c9f1
Sliman Elmansouri <elmansourisliman@gmail.com>
2023-12-01 20:10:01 +0100

    all tasks

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """A script that takes in a URL, sends a request to the URL and displays the body
 of the response."""
-import sys, requests
+import sys
+import requests
 
 if __name__ == '__main__':
 
@@ -11,4 +12,3 @@ if __name__ == '__main__':
         print("Error code: {}".format(req.status_code))
     else:
         print(req.text)
-        
