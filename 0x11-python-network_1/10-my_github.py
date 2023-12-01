530751423ea1d319e0f642901de420937dc29cec
Sliman Elmansouri <elmansourisliman@gmail.com>
2023-12-01 20:06:25 +0100

    all tasks

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """A script that takes your Github credentials (username and password)
 and uses the Github API to display your id"""
-import sys, requests
 from requests.auth import HTTPBasicAuth
+import sys
+import requests
 
 if __name__ == '__main__':
 
