5a3de15402afca09d5a7a8b4c413d1e57246d33b
Sliman Elmansouri <elmansourisliman@gmail.com>
2024-01-03 15:48:38 +0100

    requirements fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,11 +2,8 @@
 const process = require('process');
 const fs = require('fs');
 
-// the first arg is the file path
 const file = process.argv[2];
-// file content on utf8
 fs.readFile(file, 'utf8', function (err, data){
-    // if err, print error object.
     if (err) {
         console.log(err);
     } else {
